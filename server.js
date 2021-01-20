c0967ccf67490ac56dcb2ed349bfaf0186ae2b93
Luciano Junior <engluciano.junior@gmail.com>
2021-01-19 21:39:48 -0300

    Parte2

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function () {
     console.log('Serve Rodando na porta 3000')
+})
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs' )
+})
+
+app.set('view engine', 'ejs')
+
+
+app.post('/show', (req, res) => {
+    console.log(req.body)
 })
